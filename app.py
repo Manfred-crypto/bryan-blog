c040dbc19651189bc48c4dff65d8b62fec722b5d
Little Manfred <leungcy314@gmail.com>
2026-04-05 22:15:30 +0800

    fix: quote syntax error

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def login():
         if check_password_hash(datapw, password):
             return f"<h1>Success!</h1> Welcome back {username}."
         else:
-            return f"<h1 class="earthquake">YOU SHALL NOT PASS</h1>"
+            return f'<h1 class="earthquake">YOU SHALL NOT PASS</h1>'
     else:
         return f"<h1 class="earthquake">YOU SHALL NOT PASS</h1>"
 
